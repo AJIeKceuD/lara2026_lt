b1e3f0fb4c47e01eb97826aab0e0538a30db7f5f
Alexey Dukharev <alexey.dukharev@inplat.ru>
2026-08-25 04:52:25 +0300

    public_html storage + use Vite

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,6 +47,15 @@ return [
             'report' => false,
         ],
 
+	'public_html' => [
+            'driver' => 'local',
+            'root' => base_path('../public_html/storage'),
+            'url' => env('APP_URL') . '/storage',
+            'visibility' => 'public',
+            'throw' => true,
+            'report' => true,
+	],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
